77b226d1e8efd6bc974d09d630cbdbf9104fe6b0
SanghaviHarshPankajkumar <84182620+SanghaviHarshPankajkumar@users.noreply.github.com>
2025-07-08 21:31:47 +0530

    Update config.py

--- server/core/config.py
+++ server/core/config.py
@@ -3,7 +3,7 @@ Core configuration settings for file handling and model parameters.
 """
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent  / "uploads"
+BASE_DIR = Path("/tmp").resolve().parent.parent.parent  / "uploads"
 ALLOWED_EXTENSIONS = {".jpg", ".jpeg", ".png"}
 MAX_FILE_SIZE_MB = 25
 MAX_FILE_SIZE = MAX_FILE_SIZE_MB * 1024 * 1024
